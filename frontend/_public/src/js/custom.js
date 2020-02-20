a3236500685d7aaf14541ddf242f5b978f825ade
nexxtdesign <50987621+nexxtdesign@users.noreply.github.com>
2020-02-20 11:07:31 +0100

    Update custom.js

--- frontend/_public/src/js/custom.js
+++ frontend/_public/src/js/custom.js
@@ -9,12 +9,12 @@ $(function() {
     });
     
     $(document).ready(function() {
-    	$.subscribe('plugin/swEmotionLoader/onLoadEmotionFinished', function() {
+/*    	$.subscribe('plugin/swEmotionLoader/onLoadEmotionFinished', function() {
     		$('.slidenav li').on('click', function() {
     			var mySwiper = document.querySelector('.swiper-container-horizontal').swiper;
     			
     			mySwiper.slideTo($(this).index() + 1);
-    		});
+    		});*/
     	});
     	
     	// Mobile subcategories
