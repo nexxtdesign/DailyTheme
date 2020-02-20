eb4a63e9575fd7e2588ba44953c7c2f1c6df61fb
nexxtdesign <50987621+nexxtdesign@users.noreply.github.com>
2020-02-20 10:12:08 +0100

    added slick libs

--- Theme.php
+++ Theme.php
@@ -29,6 +29,8 @@ class Theme extends \Shopware\Components\Theme
         'src/aos/aos.js',
         'src/js/custom.js',
         'src/js/DAILY.js'
+        'src/js/plugins/slick.min.js'
+
     );
 
     private $fieldSetDefaults = [
